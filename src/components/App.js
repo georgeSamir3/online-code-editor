fd0aec8d0f1e0fe3b02afe7f05309f0db83637b5
georgeSamir3 <georgesamirabd@gmail.com>
2023-05-02 01:40:02 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Editor from "./Editor";
 import useLocalStorage from "../hooks/useLocalStorage";
-import Annotation from "./Annotation";
 
 function App() {
-	// Calling custom hook to save data to our local storage
-	const [html, setHtml] = useLocalStorage("html", "");
-	const [css, setCss] = useLocalStorage("css", "");
-	const [js, setJs] = useLocalStorage("js", "");
-	// state to pass the html document to the iframe on srcDoc attribute
-	const [srcDoc, setSrcDoc] = useState("");
-	let annotate = true;
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
 
-	if (html !== "" || css !== "" || js !== "") {
-		annotate = false;
-	}
-
-	// we set a timeout of 1s because our code editor will refresh
-	// after every change in code editor and hence it may cause overhead
-
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			// code which renders in bottom pane is stored here
-			// we put this variable in iframe as srcDoc
-			setSrcDoc(`
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
 			<!DOCTYPE html>
       <html lang="en">
       <head>
@@ -37,57 +24,48 @@ function App() {
       </body>
       </html>
 			`);
-		}, 1000);
+    }, 1000);
 
-		// if we updated html, css or js within 1000 ms, we dont want to create multiple setTImeout
-		// we clear the previous timeout by passing the returned integer by set TImeout and create a new timeout
-		return () => clearTimeout(timeout);
-	}, [html, css, js]);
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
-	return (
-		<React.Fragment>
-			{/* for the top html, css and js columns*/}
-			<div className="pane top-pane">
-				<Editor // passing as props
-					language="xml"
-					displayName="HTML"
-					value={html}
-					onChange={setHtml}
-				/>
-				<Editor
-					language="css"
-					displayName="CSS"
-					value={css}
-					onChange={setCss}
-				/>
-				<Editor
-					language="javascript"
-					displayName="JavaScript"
-					value={js}
-					onChange={setJs}
-				/>
-			</div>
+  return (
+    <React.Fragment>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JavaScript"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
 
-			{/* for the bottom result view pane*/}
-
-			<div className="pane bottom-pane">
-				{annotate ? <Annotation /> : ""}
-
-				<div className="pane">
-					{/** Inline frame or iframe is used to embed
-					 * another document within the current HTML document. */}
-					<iframe
-						srcDoc={srcDoc}
-						title="output"
-						sandbox="allow-scripts" // for security does not allow malicious code which access cookies or etc
-						frameBorder="0"
-						width="100%"
-						height="100%"
-					/>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+      <div className="pane bottom-pane">
+        <div className="pane">
+          <iframe
+            srcDoc={srcDoc}
+            title="output"
+            sandbox="allow-scripts"
+            frameBorder="0"
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,34 +1,26 @@
 import { useEffect, useState } from "react";
 
-// to know which app is using local storage
 const PREFIX = "write-code-project-";
 
-// initial value is the default state in our text editor
 function useLocalStorage(key, initialValue) {
-	const prefixedKey = PREFIX + key;
+  const prefixedKey = PREFIX + key;
 
-	// using function form of useState because
-	// getting data from LocalStoage is slow and we'll do it only once
-	const [value, setValue] = useState(() => {
-		// get data is JSON value
-		const jsonValue = localStorage.getItem(prefixedKey);
-		// if data exists, we return the parsed version of it
-		if (jsonValue != null) return JSON.parse(jsonValue);
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey);
+    if (jsonValue != null) return JSON.parse(jsonValue);
 
-		// if we dont have initial value, we'll return initial value
-		if (typeof initialValue === "function") {
-			return initialValue();
-		} else {
-			return initialValue;
-		}
-	});
+    if (typeof initialValue === "function") {
+      return initialValue();
+    } else {
+      return initialValue;
+    }
+  });
 
-	// If our data changes, we'll store the data into local storage as stringified version
-	useEffect(() => {
-		localStorage.setItem(prefixedKey, JSON.stringify(value));
-	}, [prefixedKey, value]);
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value));
+  }, [prefixedKey, value]);
 
-	return [value, setValue];
+  return [value, setValue];
 }
 
 export default useLocalStorage;
